d2d0a5d9c13dae6cda29148288702aeab9f47f8e
kurosujiomake <kurosujiomake@gmail.com>
2017-02-27 21:58:15 -0800

    Added PoisonIvy Boss basics
    see title
    also the script is not done but should run

---
+++ Assets/Scripts/AI/Bosses/PoisonIvy/BossMain.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMain : MonoBehaviour
+{
+    private enum BossStates { Spawning, Seek, Rest};
+    [SerializeField]
+    private BossStates bossControl;
+    public GameObject orbiters;
+    public GameObject Spawnner;
+    public bool sendToPlayer = false;
+    public bool canSeek = false;
+    public int orbiterCounter;
+    [SerializeField]
+    private int orbiterMax;
+    
+    public float spawnTimer;
+    private float cSpawnTimer;
+    public float restTimer;
+    private float cRestTimer;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        bossControl = BossStates.Rest;
+        cRestTimer = restTimer;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(bossControl == BossStates.Rest)
+        {
+            sendToPlayer = false;
+            cRestTimer -= Time.deltaTime;
+            if(cRestTimer <= 0)
+            {
+                if(canSeek)
+                {
+                    bossControl = BossStates.Seek;
+                }
+                else if (!canSeek)
+                {
+                    bossControl = BossStates.Spawning;
+                }
+            }
+        }
+        if (bossControl == BossStates.Spawning)
+        {
+            if (orbiterCounter < orbiterMax)
+            {
+                cSpawnTimer -= Time.deltaTime;
+                if (cSpawnTimer <= 0)
+                {
+
+                    Spawning();
+                    orbiterCounter++;
+                    cSpawnTimer = spawnTimer;
+                }
+
+            }
+            else
+            {
+                bossControl = BossStates.Rest;
+                canSeek = true;
+                cRestTimer = restTimer;
+            }
+
+        }
+
+        if(bossControl == BossStates.Seek)
+        {
+            SendThemToDie();
+        }
+ 
+	}
+    
+    void Spawning()
+    {
+
+        Instantiate(orbiters, Spawnner.transform.position, Spawnner.transform.rotation);
+    }
+
+    void SendThemToDie()
+    {
+        sendToPlayer = true;
+        orbiterCounter = 0;
+        cRestTimer = restTimer;
+        canSeek = false;
+        bossControl = BossStates.Rest;
+    }
+
+}
---
+++ Assets/Scripts/AI/Bosses/PoisonIvy/Orbiter.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+
+    [SerializeField]
+    private int maxHealth;
+    public int currentHealth;
+    public enum StateOfAgression { low, med, high};
+    public StateOfAgression AgroControl;
+    public enum StateOfOrb { Spawnning, Orbiting, Seeking};
+    public StateOfOrb OrbitalStatus;
+    [SerializeField]
+    private GameObject mainBoss;
+    private Transform center;
+    private Vector3 axis = Vector3.up;
+    private Vector3 orbitalPosition;
+    [SerializeField]
+    private float radius;
+    [SerializeField]
+    private float ejectSpeedMin;
+    [SerializeField]
+    private float ejectSpeedMax;
+    [SerializeField]
+    private float radiusSpd;
+    [SerializeField]
+    private float rotSpd;
+    [SerializeField]
+    private float ejectSpeed;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private float SeekingSpd;
+    public bool seekingPlayer = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentHealth = maxHealth;
+        //the main object these things orbit
+        mainBoss = GameObject.FindGameObjectWithTag("PoisonIvyMain");
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //the speed it spawns from the boss
+        ejectSpeed = Random.Range(ejectSpeedMin, ejectSpeedMax);
+        OrbitalStatus = StateOfOrb.Spawnning;
+        center = mainBoss.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //health counter
+        if(currentHealth <= 0)
+        {
+            Death();
+        }
+
+        //recieves signal from the boss
+        seekingPlayer = mainBoss.GetComponent<BossMain>().sendToPlayer;
+
+        if(seekingPlayer)
+        {
+            OrbitalStatus = StateOfOrb.Seeking;
+        }
+
+        if(OrbitalStatus == StateOfOrb.Spawnning)
+        {
+            StartingOrbit();
+        }
+        if(OrbitalStatus == StateOfOrb.Orbiting)
+        {
+            OrbitingStuff();
+        }
+        if(OrbitalStatus == StateOfOrb.Seeking)
+        {
+            SeekingPlayer();
+        }
+	}
+
+    //it gets spawned and locates the boss before spawning
+    void StartingOrbit()
+    {
+        
+        this.transform.Translate(Vector3.forward * ejectSpeed * Time.deltaTime);
+        ejectSpeed -= 2.5f * Time.deltaTime;
+        if(ejectSpeed <= 0)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(this.transform.position, Vector3.back);
+            if(Physics.Raycast(ray, out hit))
+            {
+                radius = hit.distance;
+            }
+            OrbitalStatus = StateOfOrb.Orbiting;
+        }
+    }
+
+    //actrually orbiting the boss
+    void OrbitingStuff()
+    {
+        transform.RotateAround(center.position, axis, rotSpd * Time.deltaTime);
+        orbitalPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, orbitalPosition, radiusSpd * Time.deltaTime);
+    }
+
+    //looking for the player
+    void SeekingPlayer()
+    {
+        transform.LookAt(player.transform.position);
+        transform.Translate(Vector3.forward * SeekingSpd * Time.deltaTime);
+    }
+
+    public void GetHurt(int Damage)
+    {
+        currentHealth -= Damage;
+
+    }
+
+    void Death()
+    {
+        //mainBoss.GetComponent<BossMain>().orbiterCounter--;
+        Destroy(this.gameObject);
+    }
+}
