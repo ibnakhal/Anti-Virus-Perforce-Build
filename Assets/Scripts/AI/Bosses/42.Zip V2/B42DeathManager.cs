88984f956ccc4af0161b91d3aa2bc30951246ffc
Zarthose <segmalia777@gmail.com>
2017-03-01 21:01:52 -0800

    42 death 'animation' and particle effects added.
    Changes viewable in Michael S test scene.

--- Assets/Scripts/AI/Bosses/42.Zip V2/B42DeathManager.cs
+++ Assets/Scripts/AI/Bosses/42.Zip V2/B42DeathManager.cs
@@ -14,10 +14,14 @@ public class B42DeathManager : MonoBehaviour {
     GameObject phaseFour;
     [SerializeField]
     GameObject deathExplosion;
+    [SerializeField]
+    GameObject deathImplosion;
 
     ILYBGFXBasicBehavior[] bossSpinner;
     BasicEnemyHealth[] childHealth;
     B42BasicBehaviorV2[] bossSpawner;
+
+    float spinSpeed = 1000;
 
 	// Use this for initialization
 	void Start ()
@@ -53,39 +57,224 @@ public class B42DeathManager : MonoBehaviour {
             yield return new WaitForSeconds(0.025f);
         }
 
-        yield return new WaitForSeconds(2);
-
-        if (phaseFour.activeSelf == true)
-        {
-            yield return new WaitForSeconds(1);
-            while(phaseFour.transform.localScale.x >= 0.1f)
-            {
-                phaseFour.transform.localScale -= Vector3.one * Time.deltaTime * 10;
-                yield return new WaitForSeconds(0.0001f);
-            }
-        }
-
-        if (phaseThree.activeSelf == true)
-        {
-            yield return new WaitForSeconds(0.5f);
-            while (phaseThree.transform.localScale.x >= 0.1f)
-            {
-                phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
-                yield return new WaitForSeconds(0.0001f);
-            }
-        }
-
-        if (phaseTwo.activeSelf == true)
-        {
-            yield return new WaitForSeconds(0.5f);
-            while (phaseTwo.transform.localScale.x >= 0.1f)
-            {
-                phaseTwo.transform.localScale -= Vector3.one * Time.deltaTime * 10;
-                yield return new WaitForSeconds(0.0001f);
-            }
-        }
-
         yield return new WaitForSeconds(3);
+        
+        while(phaseFour.transform.localScale.x >= 0.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+            
+        yield return new WaitForSeconds(0.5f);
+        while (phaseThree.transform.localScale.x >= 1 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        } 
+        yield return new WaitForSeconds(0.5f);
+        while (phaseFour.transform.localScale.x >= 0.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseTwo.transform.localScale.x <= 3 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseTwo.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        while (phaseThree.transform.localScale.x <= 3 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.1f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseTwo.transform.localScale.x >= 1 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseTwo.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        while (phaseThree.transform.localScale.x >= 0.5f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseThree.transform.localScale.x <= 3 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseFour.transform.localScale.x >= 0.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+        while (phaseTwo.transform.localScale.x <= 3 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseTwo.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.0001f);
+        }
+
+        spinSpeed = 3000;
+
+        while (phaseThree.transform.localScale.x <= 3 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.1f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseTwo.transform.localScale.x >= 1 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseTwo.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.5f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x <= 3 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.5f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x <= 2 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseFour.transform.localScale.x >= 0.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseTwo.transform.localScale.x <= 3 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseTwo.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x <= 3 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.1f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.Rotate(Vector3.left, spinSpeed * Time.deltaTime);
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseTwo.transform.localScale.x >= 1 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseTwo.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.5f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.right, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x <= 3 && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseThree.transform.localScale.x >= 0.5f && phaseThree.activeSelf == true)
+        {
+            phaseThree.transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+            phaseThree.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        while (phaseFour.transform.localScale.x <= 1.5f && phaseFour.activeSelf == true)
+        {
+            phaseFour.transform.localScale += Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+        while (phaseTwo.transform.localScale.x >= 0.1 && phaseTwo.activeSelf == true)
+        {
+            phaseTwo.transform.localScale -= Vector3.one * Time.deltaTime * 10;
+            yield return new WaitForSeconds(0.00001f);
+        }
+
+        yield return new WaitForSeconds(1);
+
+        Instantiate(deathImplosion, this.transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(4.5f);
 
         Instantiate(deathExplosion, this.transform.position, Quaternion.identity);
         yield return new WaitForSeconds(0.5f);
